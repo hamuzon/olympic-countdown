37caad4c276b019bf2eff1ed40d29d26d76a9c7a
hamuzon <github@hamusata.f5.si>
2026-06-21 16:56:48 +0900

    chore: update nuxt configuration settings

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -31,11 +31,6 @@ export default defineNuxtConfig({
       link: [
         { rel: 'icon', type: 'image/svg+xml', href: '/icon.svg' },
         { rel: 'icon', sizes: 'any', href: '/favicon.ico' }
-      ],
-      script: [
-        {
-          innerHTML: `if (window.location.hostname.endsWith('.')) { window.location.replace(window.location.href.replace(window.location.hostname, window.location.hostname.slice(0, -1))); }`
-        }
       ]
     }
   },
